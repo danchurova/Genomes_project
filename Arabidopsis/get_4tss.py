dc286be00d0bff289d26c150ae83d9f909161770
Anton Karamanov <ataraxer@gmail.com>
2018-05-25 00:58:57 +0300

    tts is debugged

--- Arabidopsis/get_4tss.py
+++ Arabidopsis/get_4tss.py
@@ -8,7 +8,7 @@ with open('./data/4tss_arab_mrnas.txt', 'w') as tss:
     tss.write("{}\t{}\t{}\t{}\t{}\t{}\n".format('CHROM','FRAG_START','FRAG_STOP', 'STRAND', 'DESCRIPTION', 'MODEL'))
     for line in data.readlines():
         chrom, _, kind, start, stop, _, strain, _,  name = line.strip().split('\t')
-        if kind == "protein":
+        if kind == "mRNA":
             model = name.split(';')[1].split("=")[1]
             tss.write("{}\t{}\t{}\t{}\t\"{}\"\t{}\n".format(chrom, start, stop, strain, name, model))
 
